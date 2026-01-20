8678808aac352f6b1d9e4785d1f8c5f879edd82e
NishadKolhe <nishadkolhew@gmail.com>
2026-01-20 17:54:45 -0500

    Added viewsets and API routes for Patients, Appointments, Prescriptions and Medications, tested with Postman

--- backend/prescriptions/views.py
+++ backend/prescriptions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Prescription, Medication
+from .serializers import PrescriptionSerializer, MedicationSerializer
 
-# Create your views here.
+class PrescriptionViewSet(viewsets.ModelViewSet):
+    queryset = Prescription.objects.all()
+    serializer_class = PrescriptionSerializer
+
+class MedicationViewSet(viewsets.ModelViewSet):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+
